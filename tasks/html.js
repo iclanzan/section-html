b0401570e6577bc13ec496b42bf8da8642deea69
iclanzan <iclanzan@macbook>
2013-10-28 17:30:13 -1000

    Fixed bug where the listener was being added multiple times.

--- tasks/html.js
+++ tasks/html.js
@@ -17,18 +17,19 @@ module.exports = function (grunt) {
   var readFile = grunt.file.read;
   var processTemplate = grunt.template.process;
 
+  var template;
   evt.on(['section', 'init'], function (opts) {
     if (!opts.template) {
       return;
     }
 
-    var template = readFile(opts.template);
+    template = readFile(opts.template);
     opts.pagePrototype.include = readFile;
+  });
 
-    evt.on(['section', 'render'], function process(page) {
-      var $ = cheerio.load(processTemplate(template, {data: page}), {ignoreWhitespace: true});
-      evt.emit(['section', 'render', 'html'], $, page);
-      page.body = $.html();
-    });
+  evt.on(['section', 'render'], function process(page) {
+    var $ = cheerio.load(processTemplate(template, {data: page}), {ignoreWhitespace: true});
+    evt.emit(['section', 'render', 'html'], $, page);
+    page.body = $.html();
   });
 };
